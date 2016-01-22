634be1d603630fc4aee8796d01a61758c26463d4
William Johnson <wjohnson1000@gmail.com>
2016-01-22 13:06:05 -0700

    trying to parse data from sheets

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-var app = angular.module('eventManager', [])
-app.controller('events', ['$resource', '$scope', function($scope){
+var app = angular.module('eventManager', []);
+
+app.controller('events', ['$http', '$scope', function($http, $scope){
   $scope.addEvent = false;
   $scope.sortBy = '-date'
-  var data = $resource('https://spreadsheets.google.com/feeds/list/1B15WFiVk21x2d90g0srXAGd-B7UXvdBAvrzgujASbLE/1/public/full?alt=json').get();
-  data.$promise.then(
-      function(forms){
-        console.log(forms);
+  var data = $http({
+                method: 'GET',
+                url: 'https://spreadsheets.google.com/feeds/list/1B15WFiVk21x2d90g0srXAGd-B7UXvdBAvrzgujASbLE/1/public/full?alt=json'
+              }).then(function (response){
+                // console.log(response);
+                console.log('success');
+                return response.data.feed.entry;
+              }, function (response) {
+                console.error('error');
+              }
 
-
-        $scope.events = newDataStuff
-      }
-  )
+  );
+  // console.log(data)
+data.forEach()
+  // data.$promise.then(
+  //     function(forms){
+  //       console.log(forms);
+  //
+  //
+  //       $scope.events = newDataStuff
+  //     }
+  // )
 }])
