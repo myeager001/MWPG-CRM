a50cfebfad1a7f9860e4e0ae3aa9bc8a7b625658
William Johnson <wjohnson1000@gmail.com>
2016-01-22 12:45:21 -0700

    here mike

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 var app = angular.module('eventManager', [])
-app.controller('events', function($scope){
+app.controller('events', ['$resource', '$scope', function($scope){
   $scope.addEvent = false;
   $scope.sortBy = '-date'
-})
+  var data = $resource('https://spreadsheets.google.com/feeds/list/1B15WFiVk21x2d90g0srXAGd-B7UXvdBAvrzgujASbLE/1/public/full?alt=json').get();
+  data.$promise.then(
+      function(forms){
+        console.log(forms);
+
+
+        $scope.events = newDataStuff
+      }
+  )
+}])
